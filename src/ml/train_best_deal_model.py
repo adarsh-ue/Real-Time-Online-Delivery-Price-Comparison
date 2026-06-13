de4b27839684f61cb092c1a0f04401c4efd2e99f
Ashwathykariyeri-ds <aswathy.kariyeri@ue-germany.de>
2026-06-14 00:05:02 +0200

    Update train_best_deal_model.py

--- src/ml/train_best_deal_model.py
+++ src/ml/train_best_deal_model.py
@@ -1,3 +1,8 @@
 import sqlite3
 import pandas as pd
 import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
