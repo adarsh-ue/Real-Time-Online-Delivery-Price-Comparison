1eb6d7fca77f3eeaab095c17a4461e301bb9942f
Ashwathykariyeri-ds <aswathy.kariyeri@ue-germany.de>
2026-06-14 00:05:20 +0200

    Update train_best_deal_model.py

--- src/ml/train_best_deal_model.py
+++ src/ml/train_best_deal_model.py
@@ -6,3 +6,5 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
