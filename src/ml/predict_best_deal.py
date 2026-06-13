f7a11fe32ef9e6dce73d9a930dee02c431272dec
Ashwathykariyeri-ds <aswathy.kariyeri@ue-germany.de>
2026-06-14 00:08:09 +0200

    Update predict_best_deal.py

--- src/ml/predict_best_deal.py
+++ src/ml/predict_best_deal.py
@@ -4,10 +4,8 @@ import pandas as pd
 
 MODEL_PATH = "src/ml/best_deal_model.pkl"
 
-
 def model_exists():
     return os.path.exists(MODEL_PATH)
-
 
 def predict_best_deal(product):
     model = joblib.load(MODEL_PATH)
